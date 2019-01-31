b994617b536b1d74d6a59e8bce4edfe9ac649f4e
rafactor <avila.rafael@gmail.com>
2019-01-30 23:26:51 -0500

    add user model

--- models/question.js
+++ models/question.js
@@ -1,19 +1,11 @@
-module.exports = function(sequelize, DataTypes) {
-  var Question = sequelize.define("Question", {
-    question: DataTypes.STRING,
-    type: DataTypes.STRING,
-    status: DataTypes.STRING,
-    required: DataTypes.BOOLEAN
-  });
-
-  Question.associate = function(models) {
-    // We're saying that a Question should belong to an Questionnaire
-    // A Question can't be created without an Questionnaire due to the foreign key constraint
-    Question.belongsTo(models.Questionnaire, {
-      foreignKey: {
-        allowNull: false
-      }
+module.exports = function (sequelize, DataTypes) {
+    var Question = sequelize.define('question', {
+        question: DataTypes.STRING,
+        type: DataTypes.STRING,
+        status: DataTypes.STRING,
+        required: DataTypes.BOOLEAN,
+        
     });
-  };
-  return Question;
-};
+    return Question;
+
+}
--- models/questionnaire.js
+++ models/questionnaire.js
@@ -1,27 +1,27 @@
 module.exports = function(sequelize, DataTypes) {
-  var Questionnaire = sequelize.define("Questionnaire", {
-    name: {
-      type: DataTypes.STRING,
-      defaultValue: null,
-      allowNull: false
-    },
-    type: {
-      type: DataTypes.STRING,
-      defaultValue: null,
-      allowNull: false
-    },
-    active: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true
-    }
-  });
-
-  Questionnaire.associate = function(models) {
-    // Associate Questionnaire with Questions
-    Questionnaire.hasMany(models.Question, {
-      onDelete: "cascade"
+    var Questionnaire = sequelize.define("questionnaire", {
+      name: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+        allowNull: false,
+      },
+      type: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+        allowNull: false,
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+      }
     });
-  };
 
-  return Questionnaire;
-};
+    Questionnaire.associate = function(models){
+      // Associate Questionnaire with Questions
+      Questionnaire.hasMany(models.question, {
+        onDelete: "cascade"
+      });
+    };
+
+    return Questionnaire;
+  };
