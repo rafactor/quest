53fc6b81a81690106b8f0b87e0d19abb321c9959
rafactor <avila.rafael@gmail.com>
2019-02-03 20:40:07 -0500

    basic landing layout

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,6 +5,14 @@ router.get("/", function(req, res) {
   res.render("index", {});
 });
 
+router.get("/admin", function(req, res) {
+  res.render("admin", {});
+});
+
+// router.get("/admin", function(req, res) {
+//   res.render("admin", {});
+// });
+
 router.get("/about", function(req, res) {
   res.send("im the about page!");
 });
