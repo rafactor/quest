88103451be6eaccd14cbed6c14f2318f2273b5eb
Catchem87 <moras.ashley87@gmail.com>
2019-02-06 18:42:50 -0500

    working changes

---
+++ controllers/auxiliar-controller.js
@@ -0,0 +1,18 @@
+var db = require("../models");
+
+module.exports = {
+
+  getAuxOption: function(req, res) {
+    db.AuxOptions.findAll({}).then(function(response) {
+      // console.log(response);
+      if (req.accepts("html")) {
+        var displayQuestions = response.map(function(item){
+          console.log(item);
+          return item
+        })
+        return res.render("index", {auxiliar: response})
+      };
+    });
+  },
+
+};
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,17 @@
 var express = require("express");
 var router = express.Router();
 
-var questionnaire = require("../controllers/questionnaire-controller")
+var questionnaire = require("../controllers/questionnaire-controller");
+var auxiliar = require("../controllers/auxiliar-controller");
+
 // route Questionnaires calls
 router.get("/questionnaires", questionnaire.getAll);
 router.get("/questionnaires/:id", questionnaire.getQuestionnaire);
 router.post("/questionnaires/", questionnaire.create);
 router.delete("/questionnaires/:id", questionnaire.delete);
 router.put("/questionnaires", questionnaire.update);
+
+// route Auxiliar calls
+router.get("/auxiliar", auxiliar.getAuxOption);
 
 module.exports = router;
