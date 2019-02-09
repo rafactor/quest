14525e2536e1957677124672fbc25d5334cd2a23
rafactor <avila.rafael@gmail.com>
2019-02-09 13:43:19 -0500

    fix in login router

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,11 +6,11 @@ var questionnaire = require("../controllers/questionnaire-controller");
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.render("index", {});
 });
 
-router.get("/login", function(req, res) {
+router.get("/login", function (req, res) {
   if (req.user) {
     res.render("members", {});
   }
@@ -22,22 +22,24 @@ router.get("/login", function(req, res) {
 // Here we've add our isAuthenticated middleware to this route.
 // If a user who is not logged in tries to access this route they will be redirected to the signup page
 
-router.get("/members", isAuthenticated, function(req, res) {
+router.get("/members", isAuthenticated, function (req, res) {
   if (req.user) {
     res.render("members", {});
+  } else {
+    res.render("login", {
+      registered: true
+    });
   }
-  res.render("login", {
-    registered: true
-  });
 });
 
-router.get("/admin", isAuthenticated, function(req, res) {
+
+router.get("/admin", isAuthenticated, function (req, res) {
   questionnaire
     .getAll()
-    .then(function(response) {
+    .then(function (response) {
       res.render("admin", response);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.status(500).end();
     });
 });
