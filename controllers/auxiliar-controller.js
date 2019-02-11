245f1d448427f3594f9fb11fb32a6d6d6e3eba33
Catchem87 <moras.ashley87@gmail.com>
2019-02-11 18:00:34 -0500

    display questions

--- controllers/auxiliar-controller.js
+++ controllers/auxiliar-controller.js
@@ -8,18 +8,8 @@ module.exports = {
     ]}).then(function(response) {
       var finalResponseData = {};
       console.log(JSON.stringify(response[0].dataValues, null, 2))
-
-      //  for (i = 0; i < response.length; i++) {
-      //    var auxArray = response[i].dataValues.AuxOptions;
-      //    console.log(auxArray);
-      //   for (y = 0; y < auxArray.length; y++) {
-      //     var responseArray = auxArray[y].dataValues;
-      //     console.log(responseArray);
-      //   };
-      //  };
         return res.render("partials/main/questionnaire", {
           questionData: response,
-          // answers: responseArray,
         });
     });
   },
